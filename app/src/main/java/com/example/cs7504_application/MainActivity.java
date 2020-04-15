f71ef48a2b85a5d97287c15e7371400a768cead8
Manal Agrawal <agrawal_manal@yahoo.com>
2020-04-15 15:51:45 +1200

    Fourth Commit - In this commit I have added some reference code in the Main Activity File Where I did some Java coding for the reference of the number.

--- app/src/main/java/com/example/cs7504_application/MainActivity.java
+++ app/src/main/java/com/example/cs7504_application/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.cs7504_application;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView result;
+    EditText number1, number2;
+    Button Add, Subtraction, multiply, divide;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        result = (TextView)findViewById(R.id.result);
+
+        number1 = (EditText)findViewById(R.id.number1);
+        number2=(EditText)findViewById(R.id.number2);
+
+        Add = (Button)findViewById(R.id.Add);
+        Subtraction = (Button)findViewById(R.id.Subtraction);
+        multiply = (Button)findViewById(R.id.multiply);
+        divide = (Button)findViewById(R.id.divide);
     }
 }
